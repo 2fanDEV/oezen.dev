91b66bc7d28630986cdfc72955165a2b193d42b8
2fanDev <tufan.oezen@protonmail.com>
2022-05-10 23:13:52 +0200

    homepage

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,101 @@
-
-
 <script>
-
 export default {
-  name: 'App',
-
-  components: {
-  },
+  name: "App",
+  components: {},
 
   data: () => ({
-    //
+    tab: null,
   }),
 };
 </script>
 
 <template>
-  <v-app style="background-color: #BEAFC2">
+  <v-app style="background-color: #000000; align-items: center; align-self: center; justify-content: center;">
     <v-app-bar
-    style="resize: none !important; justify-content: center; display:flex;"
+      style="
+        resize: none !important;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+      "
       app
-      dense
+      short
+      height="70px"
+      shrink-on-scroll
       outlined
       elevate-on-scroll
-      color="#484C7F"
-      elevation="5"
+      color="#000000"
+      elevation="4"
       dark
     >
-    
- 
-    <v-tabs>
-      <v-tab> HOME </v-tab>
-      <v-tab> ABOUT ME </v-tab>
-      <v-tab> SAV </v-tab>
-      <v-tab> PHOTOGRAPHY </v-tab>
-    </v-tabs>
-     </v-app-bar>
-    <v-sheet
-      style="align-self: center; align-items: center;"
-      height="1480"
-      width="1280"
-      color="#BEAFC2">
-    <div class="greeting">
-       <h1 class= "texthome">
-          Hi an alle.
-      </h1>
-       <h1 class= "texthome">
-        Ich hasse euch und mich alle gleich viel.
-        </h1>
-         <h1 class= "texthome">
-           Work in Progress
-        </h1>
-         <h1 class= "texthome">
-          Schau noch einmal in einer Woche.
-        </h1>
-    </div>
-    </v-sheet>
-    </v-app>
+      <v-tabs v-model="tab" centered dark>
+        <v-tab href="#home"> HOME </v-tab>
+        <v-tab href="#aboutme"> ABOUT ME </v-tab>
+        <v-tab href="#sav"> SAV </v-tab>
+        <v-tab href="#photography"> PHOTOGRAPHY </v-tab>
+        <v-tab href="#droptimer"> DROPTIMER </v-tab>
+      </v-tabs>
+    </v-app-bar>
 
+    <v-tabs-items
+      style="
+        align-self: center;
+        align-items: center;
+        justify-content:center;
+      "
+      v-model="tab"
+    >
+      <v-tab-item value="home" style="align-items:center;">
+        <v-sheet height="1380" width="69vw" color="#000000" style="align-self:center">
+          <div class="greeting">
+            <h1 class="texthome">home</h1>
+          </div>
+        </v-sheet>
+      </v-tab-item>
+      <v-tab-item value="aboutme">
+        <v-sheet height="1380" width="60vw" color="#000000">
+          <div class="greeting">
+            <h1 class="texthome">aboutme</h1>
+          </div>
+        </v-sheet>
+      </v-tab-item>
+      <v-tab-item value="sav">
+        <v-sheet height="1380" width="60vw" color="#000000">
+          <div class="greeting">
+            <h1 class="texthome">sav</h1>
+          </div>
+        </v-sheet>
+      </v-tab-item>
+      <v-tab-item value="photography">
+        <v-sheet height="1380" width="60vw" color="#000000">
+          <div class="greeting">
+            <h1 class="texthome">photography</h1>
+          </div>
+        </v-sheet>
+      </v-tab-item>
+      <v-tab-item value="droptimer">
+        <v-sheet height="1280" width="1280" color="#000000">
+          <div class="greeting">
+            <h1 class="texthome">droptimer</h1>
+          </div>
+        </v-sheet>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-app>
 </template>
 
-
 <style>
-
 .texthome {
-  margin-top: 0.2%;
+  margin-top: 4em;
   position: relative;
-  color: #484C7F;
+  color: white;
 }
 
 .greeting {
   display: flex;
-  margin-top: 25%;
   flex-direction: column;
   align-items: center;
+  font-size: 50px;
 }
 
-.flexgrid {
-  display:flex;
-  flex-direction: row;
-  flex-flow: row;
-  margin-left: 50vw;
-}
 </style>
